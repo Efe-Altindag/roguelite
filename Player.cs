bd8071d6690e81d39861d72d1c4adda0867fe935
efe-altindag <efealtindag@outlook.com>
2025-08-18 11:36:07 +0300

    Implement player movement and input handling in Player.cs

--- Player.cs
+++ Player.cs
@@ -3,4 +3,29 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	
+	[Export]
+	public float Speed = 200f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Vector2.Zero;
+
+		if (Input.IsActionPressed("move_right"))
+			velocity.X += 1;
+		if (Input.IsActionPressed("move_left"))
+			velocity.X -= 1;
+		if (Input.IsActionPressed("move_down"))
+			velocity.Y += 1;
+		if (Input.IsActionPressed("move_up"))
+			velocity.Y -= 1;
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
